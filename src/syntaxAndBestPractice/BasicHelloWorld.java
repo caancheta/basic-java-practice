dbe89d0aece49bbc6c61248b82737387b484d40c
Christopher Ancheta <Christopher Ancheta>
2017-04-28 10:58:27 -0400

    Init Repo with HelloWorld program

---
+++ src/syntaxAndBestPractice/BasicHelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package syntaxAndBestPractice;
+
+/**
+ * @author Christopher
+ *
+ */
+public class BasicHelloWorld {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+  }
+
+}
