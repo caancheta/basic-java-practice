0a1e989fcad89d2b4ea6316137602b31440f0fb5
caancheta <c-ancheta@live.com>
2017-04-28 11:39:43 -0400

    Re-added the HelloWorld program to init Maven Project.
    Change-Id: I88cc44628c985a9b462b55c4ed29734f2a0dc929

---
+++ src/com/github/caancheta/basicjavapractice/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * com.github.caancheta.basicjavapractice contains a group of basic practice problems for introducing Java
+ */
+package com.github.caancheta.basicjavapractice;
+
+/**
+ * This class consist of only the main method which prints out the famous phrase, "Hello World"
+ * 
+ * @author Christopher
+ *
+ */
+public class HelloWorld {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+  }
+
+}
